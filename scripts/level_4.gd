e1f73d899aa35b15d68a02f508c96a1e2c42380d
Pangeon <kamil.cecherz@gmail.com>
2024-04-26 14:56:57 +0200

    upgrade level 4, add new background soundtracks

--- scripts/level_4.gd
+++ scripts/level_4.gd
@@ -2,4 +2,18 @@ extends Game
 
 class_name Level4
 
-@onready var ice_chest: Chest = $Objects/Chest
+@onready var laser_beams: Array[LaserBeam] = [
+	$Objects/LaserBeam, $Objects/LaserBeam2, $Objects/LaserBeam3, $Objects/LaserBeam4
+]
+
+@onready var activation_plates: Array[ActivationPlate] = [
+	$Objects/ActivationPlate
+]
+
+func _on_activation_plate_chest_touch() -> void:
+	activation_plates[0].on()
+	laser_beams[2].off()
+
+func _on_activation_plate_chest_outside() -> void:
+	activation_plates[0].off()
+	laser_beams[2].on()
